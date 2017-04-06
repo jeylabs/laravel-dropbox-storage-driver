355be274336ac6a664093c15bda316385205ecec
Ratheep <ratheep.ceymplon@jeylabs.com>
2017-04-06 12:03:57 +0530

    prefix added

--- README.md
+++ README.md
@@ -29,6 +29,8 @@ Next, add the following in ```app/filesystems.php```:
         'driver' => 'dropbox',
         'secret' => env('DROPBOX_SECRET'),
         'token' => env('DROPBOX_TOKEN'),
+        'prefix' => env('DROPBOX_PREFIX'),
+        'app_url' => env('DROPBOX_APP_URL'),
     ],
 
 ],
@@ -38,6 +40,8 @@ Then, in your ```.env``` file:
 ```
 DROPBOX_SECRET=your_app_secret_key
 DROPBOX_TOKEN=your_access_token
+DROPBOX_PREFIX=your_prefix
+DROPBOX_APP_URL=your_application_url
 ```
 
 **Dealing with Dropbox for the first time? Here's the [link](https://www.dropbox.com/developers/apps/create) to create your first application and generate your app secret key and access token.**
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -19,7 +19,8 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         Storage::extend('dropbox', function ($app, $config) {
             return new Filesystem(
                 new DropboxAdapter(
-                    new DropboxClient($config['token'], $config['secret'])
+                    new DropboxClient($config['token'], $config['secret']),
+                    $this->generatePrefix($config)
                 )
             );
         });
@@ -33,5 +34,27 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function register()
     {
         //
+    }
+
+    private function generatePrefix($config)
+    {
+        $appUrl = $config['app_url'];
+        $prefix = $config['prefix'];
+        $prefixPath = $this->trimSlash($prefix);
+        if ($appUrl){
+            $urlPrfix = $this->removeHttp($appUrl);
+            $pathPrefix = $this->trimSlash($prefix);
+            $prefixPath = $urlPrfix.$pathPrefix;
+        }
+        return $prefixPath;
+    }
+
+    private function removeHttp($url) {
+        $domain =  str_replace(array('http://','https://'), '', $url).'/';
+        return $this->trimSlash($domain);
+    }
+
+    private function trimSlash($link){
+        return trim( $link, "/" ).'/';
     }
 }
